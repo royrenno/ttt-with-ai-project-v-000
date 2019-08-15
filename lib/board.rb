94adba34fa6ed71417787df32e9a925c1535e99b
roy renno <royrenno@yahoo.com>
2019-08-15 23:34:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,5 @@
 class Board
+<<<<<<< HEAD
 
 attr_accessor :cells
 
@@ -16,6 +17,24 @@ puts "-----------"
 puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
 puts "-----------"
 puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+=======
+  attr_accessor :cells
+
+  def initialize()
+    reset!
+end
+
+def reset!
+  @cells = Array.new(9, " ")
+end
+
+def display
+  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+  puts "----------"
+  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+  puts "----------"
+  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+>>>>>>> c52a0a22b1fe9ec9cb413f9f119198ad4657c138
 end
 
 def position(user_input)
@@ -39,6 +58,10 @@ def taken?(space)
 end
 
 def valid_move?(move)
+<<<<<<< HEAD
 move.to_i.between?(1,9) && !taken?(move)
+=======
+  move.to_i.between?(1, 5) && !taken?(move)
+>>>>>>> c52a0a22b1fe9ec9cb413f9f119198ad4657c138
 end
 end
